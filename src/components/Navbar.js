fbbe479c04ad634bbf8de8c8caa1be7077d6a96f
4me7h <4573r10n@gmail.com>
2023-10-14 20:30:18 -0600

    component: navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,34 +2,27 @@ import Col from "react-bootstrap/esm/Col"
 import Container from "react-bootstrap/esm/Container"
 import Row from "react-bootstrap/esm/Row"
 
-// import { NavLink } from "react-router-dom";
+import logo from "../images/logo-blanco.png";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
+        <nav className="navBar">
             <Container>
                 <Row>
                     <Col>
-                        <div className="navbar__container">
-                            <div className="navbar__col">
-                                <img className="navbar__logo" src="null" alt="logo" />
+                        <div className="navBar__container">
+                            <div className="navBar__col">
+                                <img className="navBar__logo" src={logo} alt="logo" />
                             </div>
-                            <div className="navbar__col">
-                                <ul className="navbar__nav">
-                                    <li className="navbar__nav__item"><a href="/" exact activeClassName="active">inicio</a></li>
-                                    <li className="navbar__nav__item"><a href="/">nosotros</a></li>
-                                    <li className="navbar__nav__item"><a href="/">servicios</a></li>
-                                    <li className="navbar__nav__item"><a href="/">galeria</a></li>
-                                    <li className="navbar__nav__item"><a href="/">contacto</a></li>
+                            <div className="navBar__col">
+                                <ul className="navBar__nav">
+                                    <li className="navBar__nav__item"><a href="/" exact activeClassName="active">inicio</a></li>
+                                    <li className="navBar__nav__item"><a href="/">nosotros</a></li>
+                                    <li className="navBar__nav__item"><a href="/">servicios</a></li>
+                                    <li className="navBar__nav__item"><a href="/">galeria</a></li>
+                                    <li className="navBar__nav__item"><a href="/">contacto</a></li>
                                 </ul>
                             </div>
-                            {/* <div className="navbar__col">
-                                <button className="navbar__toggle">
-                                    <span className="navbar__toggle__icon"></span>
-                                    <span className="navbar__toggle__icon"></span>
-                                    <span className="navbar__toggle__icon"></span>
-                                </button>
-                            </div> */}
                         </div>
                     </Col>
                 </Row>
